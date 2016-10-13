5421056adc78cbb1941ff6f8ba170a3499d84487
Vincent MARY <vmary@0pb.org>
2016-10-13 14:48:00 +0200

    Commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,38 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('AppBundle:default:index.html.twig');
+        return $this->render('AppBundle:default:autoecole.html.twig');
+    }
+
+    /**
+     * @Route("/autoecole", name="autoecole")
+     */
+    public function autoecoleAction(Request $request)
+    {
+        return $this->render("AppBundle:default:autoecole.html.twig");
+    }
+
+    /**
+     * @Route("/permis/{type}", requirements={"type" = ".+"}, defaults={"type" = "all"}, name="permis")
+     */
+    public function permisAction(Request $request)
+    {
+        return $this->render("AppBundle:default:permis.html.twig");
+    }
+
+    /**
+     * @Route("/financement", name="financement")
+     */
+    public function financementAction(Request $request)
+    {
+        return $this->render("AppBundle:default:financement.html.twig");
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        return $this->render("AppBundle:default:contact.html.twig");
     }
 }
