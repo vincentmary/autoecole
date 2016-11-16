478a2490cd65f9650179675310e3d4818bc26c2c
Vincent MARY <vmary@0pb.org>
2016-11-16 14:08:03 +0100

    Finish mail feature

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
@@ -53,19 +54,35 @@ class DefaultController extends Controller
     {
         $contact = new ContactType();
 
+        if (isset($_GET['type']))
+        {
+          $contact->setType($_GET['type']);
+        }
+        else
+        {
+          $contact->setType('Default');
+        }
+
         $form = $this->createForm(ContactType::class, $contact);
 
-        if ($form->handleRequest($request)->isValid()) {
+        if ($form->handleRequest($request)->isValid())
+        {
           $message = \Swift_Message::newInstance()
           ->setSubject($contact->getType())
           ->setFrom($contact->getMail())
-          ->setTo('testvmary@yopmail.com')
+          ->setTo($this->container->getParameter('contact_email'))
           ->setBody(
               $contact->getMessage(),
               'text/html'
             );
 
-          $this->get('mailer')->send($message);
+          if ($this->get('mailer')->send($message))
+          {
+            $session = $request->getSession();
+            $session->getFlashBag()->add('success', 'Votre mail a bien été envoyé. Nous vous recontacterons le plus vite possible');
+
+            return new RedirectResponse($this->get('router')->generate('homepage'));
+          }
         }
 
         return $this->render("AppBundle:default:contact.html.twig", array(
