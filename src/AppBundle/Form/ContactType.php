9cc9f856cf1749d2588cb79208290b030a6a8442
Vincent MARY <vmary@0pb.org>
2016-12-12 13:57:25 +0100

    Modify contact form

--- src/AppBundle/Form/ContactType.php
+++ src/AppBundle/Form/ContactType.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -22,9 +23,15 @@ class ContactType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('name', TextType::class)
-      ->add('mail', TextType::class)
+      ->add('name', TextType::class, array(
+          'required' => TRUE,
+          'label' => 'Nom'
+        ))
+      ->add('mail', EmailType::class, array(
+          'required' => TRUE,
+        ))
       ->add('message', TextAreaType::class, array(
+          'required' => TRUE
         ))
       ->add('save', SubmitType::class, array('label' => 'Envoyer'))
       ->getForm();
