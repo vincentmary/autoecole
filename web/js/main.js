7aaf31608377f89e22c0527b21acef0b3f7ad7b8
vincent.mary@alchimie.com <you@example.com>
2017-01-03 17:38:28 +0100

    Add inscripition steps

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,13 @@
-$(".permis-menu-item").click(function() {
-   $(".permis-liste").addClass("hidden");
-   var div = $(this).children("a").attr('href');
-   $(div).addClass('show');
-   $(div).removeClass('hidden');
-   $(".offre-title").text($(this).text());
+$(".permis-menu-item").click(function () {
+    $(".permis-liste").addClass("hidden");
+    var div = $(this).children("a").attr('href');
+    $(div).addClass('show');
+    $(div).removeClass('hidden');
+    $(".offre-title").text($(this).text());
 });
+
+$(".inscription-type").click(function () {
+    $(".inscription-list").addClass("hidden");
+    var element = $(this).find(".inscription-list").addClass('show');
+    $(this).find(".inscription-list").removeClass('hidden');
+})
